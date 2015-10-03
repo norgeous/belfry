2a7fdda5efb78b9d0173d6d161322174b54d8f86
norgeous <daniel@danielharte.co.uk>
2015-10-03 19:15:32 +0100

    add ps1exec, modified install.bat

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ A collection of useful .bat files
 
 * cupally.bat - Runs an update of all chocolatey packages (I use this on start-up)
 * gnu.bat - opens a modified cmd with access to GNU CoreUtils (http://gnuwin32.sourceforge.net/packages/coreutils.htm) for text manipulation
-* install.bat - to be put in git repos, for installing composer and node packages automatically on windows
+* install.bat - to be put in git repos, for installing composer and nodejs dependencies automatically on windows
+* ps1exec.bat - bypass restrictions on running ps1 scripts
--- install.bat
+++ install.bat
@@ -1,29 +1,75 @@
-@ECHO OFF
+@ECHO off
+
+TITLE Project installer
+COLOR 0A
+
+SET "_drive=%~d0"
+SET "_parent=%~dp0"
+SET "_parent=%_parent:~0,-1%"
+setlocal
+SET "PATH=%PATH%;%_parent%"
+SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+PUSHD "%_parent%"
+
+CLS && ECHO.
 
 REM install chocolatey
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+WHERE /Q choco
+IF %ERRORLEVEL% equ 0 (
+	ECHO # chocolatey already installed
+) ELSE (
+	ECHO # installing chocolatey
+	@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+)
 
-REM composer dependencies
-if exist composer.json (
+IF EXIST "%_parent%\composer.json" (
 
-	REM install php
-	choco install php
+	ECHO # processing composer dependencies in "%_parent%\composer.json"
+
+	REM install php and vc redist for some dll
+	SET "PATH=%PATH%;%windir:~0,2%\tools\php"
+	WHERE /Q php
+	IF %ERRORLEVEL% equ 0 (
+		ECHO # php already installed
+	) ELSE (
+		ECHO # choco install vcredist2012 php
+		choco install vcredist2012 php
+	)
 
 	REM get composer.phar
-	php -r "readfile('https://getcomposer.org/installer');" | php
+	IF EXIST "%_parent%\composer.phar" (
+		ECHO # composer.phar already installed
+	) ELSE (
+		ECHO # getting composer.phar
+		php -r "readfile('http://getcomposer.org/installer');" | php -- --disable-tls
+	)
 	
 	REM download dependencies listed in composer.json
+	ECHO # php composer.phar install
 	php composer.phar install
 
 )
 
-REM node dependencies
-if exist package.json (
-	
+IF EXIST "%_parent%\package.json" (
+
+	ECHO # processing node dependencies in "%_parent%\package.json"
+
 	REM install node
-	choco install node
+	WHERE /Q node
+	IF %ERRORLEVEL% equ 0 (
+		ECHO # node already installed
+	) ELSE (
+		ECHO # choco install nodejs
+		choco install nodejs
+	)
 
 	REM download dependencies listed in package.json
-	npm install
+	ECHO # npm install
+	CALL npm install
 
 )
+
+ECHO. && PAUSE
+
+POPD
+EXIT /B 0
